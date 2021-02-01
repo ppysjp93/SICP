189c2f44a2d623ba1e131244d70722e224aec0b4
Samuel Palmer <samuelpalmer@Samuels-MacBook-Air.local>
2021-02-01 19:10:33 +0000

    Newtons method 2 practice

--- Chapter1/NewtonsMethod2.scm
+++ Chapter1/NewtonsMethod2.scm
@@ -6,23 +6,21 @@
 (define (improve guess x)
   (average guess (/ x guess)))
 
-(define (average x y)
-  (/ (+ x y) 2))
+(define (average x y) 
+ (/ (+ x y) 2))
 
-(define (good-enough? guess x)
-(< (abs (- (square guess) x)) 0.001))
+(define (guess-good-enough? guess x) 
+  (< (abs (- (square guess) x)) 0.001))
 
-(define (sqrt x)
+(define (sqrt x) 
   (sqrt-iter 1.0 x))
 
-(define (abs x)
-  (if (< x 0)
-    (- x)
-    x))
+(define (abs x) 
+  (if (< x 0) 
+      (- x)
+      x))
 
 (define (square x) 
   (* x x))
 
 (sqrt 9)
-
-
