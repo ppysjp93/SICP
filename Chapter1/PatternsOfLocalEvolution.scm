f84d3dc56dd8454147d889c24ba14b78b0848182
Samuel Palmer <samuelpalmer@Samuels-MacBook-Air.local>
2021-02-07 19:20:07 +0000

    Newtons method 2 practice

--- Chapter1/PatternsOfLocalEvolution.scm
+++ Chapter1/PatternsOfLocalEvolution.scm
@@ -10,4 +10,21 @@
 
 ; The second pattern we are going to look at is a 'linear iterative process'
 
+(define (factorial n) 
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count) 
+  (if (< max-count counter)
+    product
+    (fact-iter (* product counter) 
+               (+ counter 1) 
+               max-count)))
+
+(factorial 4)
+
+; max-count isn't in brackets because it is a value straight up.
+; putting parentheses around it is the same as the issues we faced in 
+; exercise 1.5, when we found because scheme is using the substituional 
+; model for calculations then it can't evaluate (p) where p is a primitive of
+; some sort
 
