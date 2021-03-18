5c5472383d09ae7eb04b6fef87937991dbddfafb
Samuel Palmer <samuelpalmer@Samuels-MacBook-Air.local>
2021-03-18 21:38:01 +0000

    Add Ex2_1-2.scm to Chapter 2

--- Chapter2/RepresentingRationals.scm
+++ Chapter2/RepresentingRationals.scm
@@ -18,10 +18,58 @@
 
 (define one-third (make-rat 1 3))
 
+
+(define (add-rat x y) 
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y) 
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
 (print-rat (add-rat one-half one-third))
 
 (print-rat (mul-rat one-half one-third))
 
 (print-rat (add-rat one-third one-third))
+
+
+(define (gcd x y) 
+  (if (= y 0) 
+      x
+      (gcd y (remainder x y))))
+
+(define (make-rat n d) 
+  (cons (/ n (gcd n d))
+        (/ d (gcd n d))))
+
+
+; We can upgrade this further by using a let :)
+
+(define (make-rat n d) 
+ (let ((g (gcd n d))) 
+   (cons (/ n g) (/ d g))))
+
+
+(define one-third (make-rat 1 3))
+
+(print-rat one-half)
+
+(define (normalise n d) 
+  (cond ((< d 0) (* d -1)) 
+        ((= b 2) 2) 
+        (else 3)))
+
+(define (abs x)
+(if (< x 0) 
+    (- x) 
+    x))
+
+
+
+
+
+
 
 
